4044db2a860fa6ef72bcdf84a01477940d1bf174
P. Christopher Bowers <pcbowers@gmail.com>
2022-02-28 14:06:44 -0500

    feat: :sparkles: Decreased Bundle Size & Improved Media Breaks
    Decreased bundle size by removing JS code when not in use. Also added additional media break for an extra small navigation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,9 +39,10 @@ module.exports = {
     },
     screens: {
       xs: '410px',
+      navXS: '500px',
       sm: '640px',
       md: '768px',
-      navSmall: '870px',
+      navMD: '880px',
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px'
